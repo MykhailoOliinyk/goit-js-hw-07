82ddeb3446c0d486106b84342849ef0cc00d4768
MykhailoOliinyk <alejjnikmikhail@gmail.com>
2021-10-25 17:06:36 +0200

    212

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
+const createitem = galleryItems
+  .map(
+    (el, index) =>
+      `<div class="gallery__item">
+  <a class="gallery__link" href="${el.original}">
+    <img
+      class="gallery__image"
+      src="${el.preview}"
+      data-source="${el.original}"
+      data-index="${index}"
+      alt="${el.description}"
+    />
+  </a>
+</div>`,
+  )
+  .join('');
+gelleryRef.insertAdjacentHTML('beforeend', createitem);
 console.log(galleryItems);
